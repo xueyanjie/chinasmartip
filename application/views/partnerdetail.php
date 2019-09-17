e3cd7ef2484cdd9bd037a72b18471fe9b0b8e0a3
xueyanjie <940358344@qq.com>
2019-09-17 21:19:52 +0800

    样式修改

--- application/views/partnerdetail.php
+++ application/views/partnerdetail.php
@@ -20,10 +20,10 @@
 	.partenterAva{position: relative;width: 173px;float: left}
 	.partenterAva .imgava{display:block;width: 125px;height: 125px;border-radius: 50%;-webkit-border-radius: 50%;border:2px solid #e3cbab;overflow: hidden;position: relative;z-index: 2;}
 	.avabg{width: 139px;height: 142px;background: url(<?php echo base_url();?>static/i/avabg.png) no-repeat 50% 50%;position: absolute;top: 0px;left: 0;}
-	.partinfo .p2224{font-size: 22px;height: 30px;color:#d2ac79;padding-top: 23px;padding-bottom: 13px; }
-	.partinfo .p1624{font-size: 16px;line-height: 30px;}
+	.partinfo .p2224{font-size: 22px;height: 24px;color:#d2ac79;padding-top: 23px;padding-bottom: 13px; }
+	.partinfo .p1624{font-size: 16px;line-height: 24px;}
 	.color9{color: #999999}
-	.partnerDetailLi{padding-left: 15px;position: relative;font-size: 16px;line-height: 24px;color: #999999;}
+	.partnerDetailLi{padding-left: 15px;position: relative;font-size: 16px;line-height: 30px;color: #999999;}
 	.partnerDetailLi::before{content: "";width: 5px;height: 5px;border-radius: 50%;background: #999;position: absolute;top:10px;left: 5px;}
 </style>
 </head>
@@ -54,7 +54,7 @@
 								<p class="p1624 color9">专利代理人</p>
 							</div>
 						</div>
-						<div class="partnerDetailCon">
+						<div class="partnerDetailCon" style="font-size: 16px;line-height: 30px;">
 							<p class="partnerDetailLi">10 年执业经验</p>
 							<p class="partnerDetailLi">10 年执业经验</p>
 							<p class="partnerDetailLi">10 年执业经验</p>
