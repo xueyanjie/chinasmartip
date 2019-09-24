68e2a7d473a11c3975bf4a7648d853cbb64bbaea
xueyanjie <940358344@qq.com>
2019-09-24 20:17:24 +0800

    人物信息修改

--- application/views/commonnavigation.php
+++ application/views/commonnavigation.php
@@ -3,7 +3,7 @@
         var html = '<a class="backindex fl" href="<?php echo base_url();?>/Main/navlist">首页</a>';
         html += '<a class="backindex fr" href="'+toPageName+'">EN</a>';
         document.write(html);
-    } else if (lang = 'EN') {
+    } else if (lang == 'EN') {
         var html = '<a class="backindex fl" href="<?php echo base_url();?>/Main/navlist_en">Home</a>';
         html += '<a class="backindex fr" href="'+toPageName+'">中文</a>';
         document.write(html);
