7ea75ddfb0905c4571c45fcd2bc3b926c7d1ac5b
xueyanjie <940358344@qq.com>
2019-09-27 09:17:07 +0800

    后台登录功能开发

--- application/views/admin/admin_cate.php
+++ application/views/admin/admin_cate.php
@@ -61,10 +61,10 @@
             </table>
             <div id="div_pager">
                 <div class="btn-group" role="group" aria-label="...">
-                    <button type="button" class="btn btn-default"><a href="/AdminNews/index?pn=1&pz=<?php echo $pz;?>">首页</a></button>
-                    <button type="button" class="btn btn-default"><a href="/AdminNews/index?pn=<?php echo intval($pn)>1 ? intval($pn)-1 : 1; ?>&pz=<?php echo $pz;?>">上一页</a></button>
-                    <button type="button" class="btn btn-default"><a href="/AdminNews/index?pn=<?php echo intval($pn)<intval($pm) ? intval($pn)+1 : $pm; ?>&pz=<?php echo $pz;?>">下一页</a></button>
-                    <button type="button" class="btn btn-default"><a href="/AdminNews/index?pn=<?php echo $pm; ?>&pz=<?php echo $pz;?>">末页</a></button>
+                    <button type="button" class="btn btn-default"><a href="/AdminCate/index?pn=1&pz=<?php echo $pz; ?>">首页</a></button>
+                    <button type="button" class="btn btn-default"><a href="/AdminCate/index?pn=<?php echo intval($pn)>1 ? intval($pn)-1 : 1; ?>&pz=<?php echo $pz;?>">上一页</a></button>
+                    <button type="button" class="btn btn-default"><a href="/AdminCate/index?pn=<?php echo intval($pn)<intval($pm) ? intval($pn)+1 : $pm; ?>&pz=<?php echo $pz;?>">下一页</a></button>
+                    <button type="button" class="btn btn-default"><a href="/AdminCate/index?pn=<?php echo $pm; ?>&pz=<?php echo $pz;?>">末页</a></button>
                 </div>
             </div>
         </div>
