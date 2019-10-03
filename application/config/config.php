a45ea3adacb9094bc62543fe4d3462af70340551
xueyanjie <940358344@qq.com>
2019-10-03 18:44:11 +0800

    端口修改

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://www.chinasmartip.com:8080/';  //base url
+//$config['base_url'] = 'http://www.chinasmartip.com:8080/';  //base url
+$config['base_url'] = 'http://www.chinasmartip.com/';  //base url
 
 /*
 |--------------------------------------------------------------------------
